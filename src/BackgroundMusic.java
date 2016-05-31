ff80713807aa60178979583f817a48d292306141
Marcin Waszak <M.Waszak@stud.elka.pw.edu.pl>
2016-05-31 03:37:41 +0200

    Fixed MIDI path issue in standalone .jar file

--- src/BackgroundMusic.java
+++ src/BackgroundMusic.java
@@ -1,8 +1,12 @@
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
 
+import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequencer;
@@ -10,33 +14,36 @@ import javax.sound.midi.Sequencer;
 public class BackgroundMusic {
 	Sequencer sequencer;
 	String midiFile;
-	
+
 	public BackgroundMusic() {
 		try {
 			sequencer = MidiSystem.getSequencer();
 		} catch (MidiUnavailableException e) {
 			e.printStackTrace();
 		}
-		midiFile = "src/sound/music.mid";
-	}	
+		midiFile = "sound/music.mid";
+	}
 
-	public void play() throws Exception {
+	public void play() {
 		// Obtains the default Sequencer connected to a default device.
-	
+
 		// Opens the device, indicating that it should now acquire any
-	    // system resources it requires and become operational.
-		sequencer.open();
+		// system resources it requires and become operational.
 
-	    // create a stream from a file
-	    InputStream is = new BufferedInputStream(new FileInputStream(new File(midiFile)));
+		URL url = this.getClass().getClassLoader().getResource(midiFile);
+		try {
+			sequencer = MidiSystem.getSequencer();
+			sequencer.setSequence(MidiSystem.getSequence(url));
+			sequencer.open();
+			sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
+			sequencer.start();
 
-	    // Sets the current sequence on which the sequencer operates.
-	    // The stream must point to MIDI file data.
-	    sequencer.setSequence(is);
-	    sequencer.setLoopCount(Sequencer.LOOP_CONTINUOUSLY);
-
-	    // Starts playback of the MIDI data in the currently loaded sequence.
-	    sequencer.start();
-
+		} catch (MidiUnavailableException ex) {
+			System.err.println("Midi Unavailable! " + ex.toString());
+		} catch (InvalidMidiDataException ex) {
+			System.err.println("Midi Invalid! " + ex.toString());
+		} catch (IOException ex) {
+			System.err.println("File Not Found! " + ex.toString());
+		}
 	}
 }
